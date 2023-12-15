4558355878de77a8886fa5a8e96e6264f268e2e9
YouSeongJung <seongjung.you@chemtopia.net>
2023-12-15 16:42:05 +0900

    nextMSDS 프론트 프로젝트 세팅완료 및 메인페이지 웹 디자인 완료

--- src/app/page.js
+++ src/app/page.js
@@ -2,111 +2,138 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className='main-top'>
+        <div className='main-top-slider'>
+          <div className='main-slider'><img src="/main-img/main-slide/main_img01.png"/></div>
+          <div className='main-slider'><img src="/main-img/main-slide/main_img02.png"/></div>
+          <div className='main-slider'><img src="/main-img/main-slide/main_img03.png"/></div>
+        </div>
+        <div className='main-top-right'>
+          <div className='main-content-box'>
+            <div className='main-content-box-item'>
+              <div className='main-content-item-header'>규제별 취급 MSDS <img src="/main-img/common/btn_more.png" /></div>
+              <div className='main-content-box-body'>
+                <table className='main-gosi-msds-table'>
+                  <tbody>
+                    <tr>
+                      <th rowSpan={3}>화관법</th>
+                      <td>유독물질</td>
+                      <td>3018건</td>
+                    </tr>
+                    <tr>
+                      <td>사고대비물질</td>
+                      <td>1661건</td>
+                    </tr>
+                    <tr>
+                      <td>취급제한물질</td>
+                      <td>1206건</td>
+                    </tr>
+                  </tbody>
+                  <tbody>
+                    <tr>
+                      <th rowSpan={3}>산안법</th>
+                      <td>노출기준설정물질</td>
+                      <td>4476건</td>
+                    </tr>
+                    <tr>
+                      <td>관리대상유해물질</td>
+                      <td>5072건</td>
+                    </tr>
+                    <tr>
+                      <td>작업환경측정대상유해인자</td>
+                      <td>4590건</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <div className='main-content-box-item'>
+              <div className='main-content-item-header'>부서별 MSDS 관리 <img src="/main-img/common/btn_more.png" /></div>
+              <div>
+                <table>
+                  <thead>
+                  <tr>
+                    <th>
+                      <div>구분</div>
+                    </th>
+                    <th>
+                      <div>제품</div>
+                    </th>
+                    <th>
+                      <div>상품</div>
+                    </th>
+                    <th>
+                      <div>원/부원료</div>
+                    </th>
+                    <th>
+                      <div>반제품</div>
+                    </th>
+                    <th>
+                      <div>기타</div>
+                    </th>
+                  </tr>
+                </thead>
+								<tbody className='dept-msds-table-tbody'>
+                  <tr>
+                    <td>관리부</td>
+                    <td>2864</td>
+                    <td>23</td>
+                    <td>964</td>
+                    <td>2</td>
+                    <td>6</td>
+                  </tr>
+                  <tr>
+                    <td>경영본부<br/>산업안전보건팀 </td>
+                    <td>175</td>
+                    <td>0</td>
+                    <td>5</td>
+                    <td>0</td>
+                    <td>0</td>
+                    </tr>
+                  <tr>
+                    <td>한화토탈</td>
+                    <td>142</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                  </tr>
+                </tbody>
+                </table></div>
+            </div>
+            <div className='main-content-box-item'>
+              <div className='main-content-item-header'>법령업데이트 <img src="/main-img/common/btn_more.png" /></div>
+              <div className='main-content-box-body main-content-law-update'>
+
+              </div>
+            </div>
+            <div className='main-content-box-item'>
+              <div className='main-content-item-header'>MSDS 변경 제출 및 영업비밀 유효기간 알림</div>
+            </div>
+          </div>
+          <div className='main-notice-div'>
+            <div className='main-notice-div-header'>공지사항 <img src="/main-img/common/btn_more.png" /></div>
+            <div className='main-notice-div-body'>
+              <div>
+                <label>패치노트</label><label>2023-12-22</label>
+              </div>
+              <div>
+                <label>Dr.MSDS 운영안내</label><label>2022-10-09</label>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className='main-bottom'>
+        <img src="/main-img/main-slide/img_slide02.png" />
+        <img src="/main-img/main-slide/img_slide04.png" />
+        <img src="/main-img/main-slide/img_slide05.png" />
+        <img src="/main-img/main-slide/img_slide06.png" />
+        <img src="/main-img/main-slide/img_slide07.png" />
+        <img src="/main-img/main-slide/img_slide08.png" />
+        <img src="/main-img/common/ic_prev.png"/>
+        <img src="/main-img/common/ic_next.png"/>
       </div>
     </main>
   )
